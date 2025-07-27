a9e93118f365a3573065d54b56ddf2be14dcc507
2DC <kkts9308@gmail.com>
2025-07-27 13:05:00 +0900

    :art: add style top menu bar

--- src/shared/widgets/layout/main-header.tsx
+++ src/shared/widgets/layout/main-header.tsx
@@ -1,10 +1,16 @@
 export const MainHeader = () => {
   return (
-    <header className="font-cafe24 base-container sticky top-0 z-10 h-header bg-white">
+    <header className="base-container sticky top-0 z-10 h-header bg-white">
       <ul className="px-5 md:px-10 lg:px-16 h-header grid grid-cols-3 justify-between items-center">
-        <li className="text-left">태수&지영</li>
-        <li className="text-center">오시는 길</li>
-        <li className="text-right">연락처</li>
+        <li className="text-left transition hover:scale-105 font-semibold active:scale-95 cursor-pointer">
+          태수&지영
+        </li>
+        <li className="text-center transition hover:scale-105 font-semibold active:scale-95 cursor-pointer">
+          오시는 길
+        </li>
+        <li className="text-right transition hover:scale-105 font-semibold active:scale-95 cursor-pointer">
+          연락처
+        </li>
       </ul>
     </header>
   );
