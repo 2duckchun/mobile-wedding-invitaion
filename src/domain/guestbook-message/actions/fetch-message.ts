d7a80392c27b44be3e3cb53e142f31e4bd604f0e
2DC <kkts9308@gmail.com>
2025-07-29 23:05:13 +0900

    :art: add fetch-message api

---
+++ src/domain/guestbook-message/actions/fetch-message.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function fetchMessages(page: number = 1, pageSize: number = 5) {
+  const supabase = await createClient();
+
+  const from = (page - 1) * pageSize;
+  const to = from + pageSize - 1;
+
+  const { data, error, count } = await supabase
+    .from("guestbook_messages")
+    .select("id, name, message, created_at", { count: "exact" })
+    .order("created_at", { ascending: false })
+    .range(from, to);
+
+  if (error) {
+    return {
+      success: false,
+      message: "메시지 조회에 실패했습니다.",
+    } as const;
+  }
+
+  return {
+    success: true,
+    message: "메시지 조회에 성공했습니다.",
+    data,
+    total: count,
+  } as const;
+}
+
+// "use server";
+
+// import { createClient } from "@/utils/supabase/server";
+
+// export async function fetchMessages() {
+//   const supabase = await createClient();
+//   const { data, error } = await supabase
+//     .from("guestbook_messages")
+//     .select("id, name, message, created_at")
+//     .order("created_at", { ascending: false });
+
+//   if (error) {
+//     return {
+//       success: false,
+//       message: "메시지 조회에 실패했습니다.",
+//     } as const;
+//   }
+
+//   return {
+//     success: true,
+//     message: "메시지 조회에 성공했습니다.",
+//     data,
+//   } as const;
+// }
