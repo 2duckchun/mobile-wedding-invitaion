8b0851e0266735c5ccf835915d40311ac7c86046
2DC <kkts9308@gmail.com>
2025-07-29 23:05:01 +0900

    :art: add submit-message api

---
+++ src/domain/guestbook-message/actions/submit-message.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { hashPassword } from "@/utils/hash-password";
+import { createClient } from "@/utils/supabase/server";
+
+export async function submitMessage(
+  name: string,
+  message: string,
+  password: string
+) {
+  const supabase = await createClient();
+  const password_hash = hashPassword(password);
+
+  const { error } = await supabase
+    .from("guestbook_messages")
+    .insert([{ name, message, password_hash }]);
+
+  if (error) {
+    return {
+      success: false,
+      message: "메시지 전송에 실패했습니다.",
+    } as const;
+  }
+
+  return {
+    success: true,
+    message: "메시지 전송에 성공했습니다.",
+  } as const;
+}
