8571ff02a0593dba0ba7ac1dd7adac38e6256bf0
2DC <kkts9308@gmail.com>
2025-07-30 22:24:20 +0900

    :art: change use-get-guestbook-message to custom use query

--- src/domain/guestbook-message/hooks/use-get-guestbook-message.ts
+++ src/domain/guestbook-message/hooks/use-get-guestbook-message.ts
@@ -1,49 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
+import { useQuery } from "@/shared/hooks/use-query";
 import { fetchMessages } from "../actions/fetch-message";
-import { GuestbookMessage } from "../schema";
 
-type GetGuestBookMessage = {
-  currentPage: number;
-  pageSize: number;
-};
-
-export const useGetGuestbookMessage = ({
-  currentPage,
-  pageSize,
-}: GetGuestBookMessage) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [messageList, setMessageList] = useState<
-    Pick<GuestbookMessage, "id" | "name" | "message" | "created_at">[]
-  >([]);
-  const [total, setTotal] = useState(0);
-
-  const loadMessages = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const res = await fetchMessages(currentPage, pageSize);
-      if (res.success) {
-        setMessageList(res.data);
-        setTotal(res.total);
-      } else {
-        setError(new Error(res.message));
-      }
-    } catch (error) {
-      setError(error as Error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [currentPage, pageSize]);
-
-  useEffect(() => {
-    loadMessages();
-  }, [currentPage, pageSize, loadMessages]);
-
-  return {
-    data: messageList,
-    total,
+export const useGetGuestbookMessage = (
+  currentPage: number,
+  pageSize: number
+) => {
+  const {
+    data: messages,
     isLoading,
     error,
-    refetch: loadMessages,
+    refetch,
+  } = useQuery({
+    queryKey: [currentPage, pageSize],
+    queryFn: async () => {
+      const res = await fetchMessages(currentPage, pageSize);
+      if (!res.success) throw new Error(res.message);
+      return res;
+    },
+    initialData: { data: [], total: 0, success: true, message: "" },
+  });
+
+  return {
+    messages,
+    isLoading,
+    error,
+    refetch,
   };
 };
